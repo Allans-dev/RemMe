12ba8d426f1fd03f60b87c2028e31d80279bb9f2
Allan.W.C <allan_195@hotmail.com>
2017-10-22 13:37:00 +1100

    - added sass and bourbon-neat to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,81 @@
+// const path = require('path');
+//
+// module.exports = {
+//   context: path.join(__dirname, 'src'),
+//   entry: [
+//     './index.js',
+//   ],
+//   output: {
+//     path: path.join(__dirname),
+//     filename: 'bundle.js',
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.jsx?$/,
+//         exclude: /node_modules/,
+//         use: [
+//           'babel-loader',
+//         ],
+//       },
+//     ],
+//   },
+//   resolve: {
+//     modules: [
+//       path.join(__dirname, 'node_modules'),
+//     ],
+//   },
+// };
+
 const path = require('path');
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+     filename: "main.css",
+     disable: process.env.NODE_ENV === "development"
+ });
 
 module.exports = {
   context: path.join(__dirname, 'src'),
-  entry: [
-    './index.js',
-  ],
-  output: {
-    path: path.join(__dirname),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
-        ],
+      entry: [
+          './main.js',
+      ],
+      output: {
+          path: path.join(__dirname),
+          filename: 'bundle.js',
       },
-    ],
-  },
-  resolve: {
-    modules: [
-      path.join(__dirname, 'node_modules'),
-    ],
-  },
-};
+      module: {
+          rules: [
+              {
+                  test: /\.js$/,
+                  exclude: /node_modules/,
+                  use: 'babel-loader',
+                  include: path.join(__dirname, 'src')
+              },
+              {
+                  test: /\.s?css$/,
+                  use: extractSass.extract({
+                      use: [
+                          { loader: "css-loader" },
+                          {
+                              loader: "sass-loader",
+                              options: {
+                                  includePaths: [require("bourbon-neat").includePaths]
+                              }
+                          }
+                          ],
+                      fallback: "style-loader"
+                  }),
+                  include: path.join(__dirname, 'style')
+              },
+          ]
+      },
+      plugins: [
+          extractSass
+      ],
+      resolve: {
+          modules: [
+              path.join(__dirname, 'node_modules'),
+          ],
+      },
